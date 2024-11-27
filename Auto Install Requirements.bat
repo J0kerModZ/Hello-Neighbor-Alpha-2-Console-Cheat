f86b3a8364a97f911c46dd099ee66269652ce953
J0kerModZ <167721907+J0kerModZ@users.noreply.github.com>
2024-11-27 14:55:52 -0500

    Source (With Both .bat Files)

---
+++ Auto Install Requirements.bat
@@ -0,0 +1,6 @@
+@echo off
+pip install keyboard
+pip install pyautogui
+echo.
+echo All Done! Please Open The .py File
+pause
---
+++ Download Python (ONLY IF NEEDED).bat
@@ -0,0 +1,3 @@
+curl -o python-installer.exe https://www.python.org/ftp/python/3.12.0/python-3.12.0-amd64.exe
+python-installer.exe /quiet InstallAllUsers=1 PrependPath=1
+python --version
